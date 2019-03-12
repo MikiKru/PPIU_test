6f055be7fd513eca9e43b2fc00b9d2fd3365b272
MikiKru <michal_kruczkowski@o2.pl>
2019-03-12 09:34:48 +0100

    commit from dev2

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -14,9 +14,6 @@ public class JavaApplication1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Hello GIT");
-        System.out.println("Hello GIT");
-    }
+
     
 }
--- src/javaapplication1/NewClass.java
+++ src/javaapplication1/NewClass.java
@@ -5,10 +5,15 @@
  */
 package javaapplication1;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author 109pro
  */
 public class NewClass {
-    
+    public static void main(String[] args) {
+        LocalDate ld = LocalDate.now();
+        System.out.println(ld);
+    }
 }
